05738e95a8464b55cc408920c03ace1de4f63328
Haruki Yano <haruki.y.11g@gmail.com>
2023-03-26 16:29:31 +0900

    Update PropertyListDebugPageBase.

--- Assets/UnityDebugSheet/Runtime/Extensions/Unity/PropertyListDebugPageBase.cs
+++ Assets/UnityDebugSheet/Runtime/Extensions/Unity/PropertyListDebugPageBase.cs
@@ -17,11 +17,20 @@ namespace UnityDebugSheet.Runtime.Extensions.Unity
     {
         private readonly Dictionary<string, int> _cellIds = new Dictionary<string, int>();
 
-        private readonly Dictionary<string, ButtonCellModel> _cellModels = new Dictionary<string, ButtonCellModel>();
+        private readonly Dictionary<string, ButtonCellModel> _buttonCellModels =
+            new Dictionary<string, ButtonCellModel>();
+
         private IReadOnlyDictionary<string, PropertyInfo> _propertyInfos;
         private Type TargetClassType => typeof(TTargetClass);
 
+        public virtual List<string> IgnoreTargetPropertyNames { get; } = new List<string>();
         public virtual List<string> UpdateTargetPropertyNames { get; } = new List<string>();
+
+        public virtual Dictionary<string, SubPageInfo> SubPageTargetPropertyNameToInfoMap { get; } =
+            new Dictionary<string, SubPageInfo>();
+
+        public virtual BindingFlags BindingFlags { get; } = BindingFlags.Public | BindingFlags.Static;
+        public virtual object TargetObject { get; } = null;
 
 
 #if UDS_USE_ASYNC_METHODS
@@ -45,7 +54,7 @@ namespace UnityDebugSheet.Runtime.Extensions.Unity
 
         private IReadOnlyDictionary<string, PropertyInfo> GetPropertyInfos()
         {
-            var propertyInfos = TargetClassType.GetProperties(BindingFlags.Public | BindingFlags.Static);
+            var propertyInfos = TargetClassType.GetProperties(BindingFlags);
             return propertyInfos
                 .Where(propertyInfo => propertyInfo.GetCustomAttribute<ObsoleteAttribute>() == null)
                 .ToDictionary(x => x.Name);
@@ -55,7 +64,7 @@ namespace UnityDebugSheet.Runtime.Extensions.Unity
         {
             foreach (var targetPropertyName in UpdateTargetPropertyNames)
             {
-                if (!_cellModels.TryGetValue(targetPropertyName, out var cellModel)
+                if (!_buttonCellModels.TryGetValue(targetPropertyName, out var cellModel)
                     || !_propertyInfos.TryGetValue(targetPropertyName, out var propertyInfo))
                     continue;
 
@@ -71,33 +80,50 @@ namespace UnityDebugSheet.Runtime.Extensions.Unity
             foreach (var cellId in _cellIds.Values)
                 RemoveItem(cellId);
             _cellIds.Clear();
-            _cellModels.Clear();
+            _buttonCellModels.Clear();
 
             foreach (var propertyInfo in _propertyInfos.Values)
             {
                 var propertyName = propertyInfo.Name;
+                if (IgnoreTargetPropertyNames.Contains(propertyName))
+                    continue;
+
                 if (!string.IsNullOrEmpty(searchString) &&
                     propertyName.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) == -1)
                     continue;
 
-                var buttonCellModel = new ButtonCellModel(true);
-                buttonCellModel.CellTexts.Text = propertyName;
-                buttonCellModel.CellTexts.SubText = CreatePropertyDescription(propertyInfo);
-                var itemId = AddButton(buttonCellModel);
-                buttonCellModel.Clicked += () =>
+                int itemId;
+
+                if (SubPageTargetPropertyNameToInfoMap.TryGetValue(propertyName, out var subPageInfo))
                 {
-                    var cell = GetCellIfExists(itemId);
-                    var cellRectTrans = (RectTransform)cell.transform;
-                    DebugSheet.Instance.BalloonButton.Show(cellRectTrans, "Copy",
-                        () =>
-                        {
-                            GUIUtility.systemCopyBuffer =
-                                $"{buttonCellModel.CellTexts.Text}: {buttonCellModel.CellTexts.SubText}";
-                        });
-                };
+                    var pageLinkButtonCellModel = new PageLinkButtonCellModel(false);
+                    pageLinkButtonCellModel.CellTexts.Text = propertyName;
+                    pageLinkButtonCellModel.ShowArrow = true;
+                    pageLinkButtonCellModel.PageType = subPageInfo.Type;
+                    pageLinkButtonCellModel.OnLoad += x => subPageInfo.OnLoad?.Invoke(x);
+                    itemId = AddPageLinkButton(pageLinkButtonCellModel);
+                }
+                else
+                {
+                    var buttonCellModel = new ButtonCellModel(true);
+                    buttonCellModel.CellTexts.Text = propertyName;
+                    buttonCellModel.CellTexts.SubText = CreatePropertyDescription(propertyInfo);
+                    itemId = AddButton(buttonCellModel);
+                    buttonCellModel.Clicked += () =>
+                    {
+                        var cell = GetCellIfExists(itemId);
+                        var cellRectTrans = (RectTransform)cell.transform;
+                        DebugSheet.Instance.BalloonButton.Show(cellRectTrans, "Copy",
+                            () =>
+                            {
+                                GUIUtility.systemCopyBuffer =
+                                    $"{buttonCellModel.CellTexts.Text}: {buttonCellModel.CellTexts.SubText}";
+                            });
+                    };
+                    _buttonCellModels.Add(propertyName, buttonCellModel);
+                }
 
                 _cellIds.Add(propertyName, itemId);
-                _cellModels.Add(propertyName, buttonCellModel);
             }
 
             Reload();
@@ -105,15 +131,40 @@ namespace UnityDebugSheet.Runtime.Extensions.Unity
 
         private string CreatePropertyDescription(PropertyInfo propertyInfo)
         {
+            var propertyName = propertyInfo.Name;
+
+            var value = propertyInfo.GetValue(TargetObject);
+            if (TryGetOverridePropertyDescription(propertyName, value, out var description))
+                return description;
+            
             if (!propertyInfo.PropertyType.IsArray)
-                return propertyInfo.GetValue(null).ToString();
+                return value?.ToString();
 
             var array = (Array)propertyInfo.GetValue(null);
-            var valueDescriptions = array.Cast<object>().Select(x => x.ToString());
-            var description = string.Join(", ", valueDescriptions);
+            var valueDescriptions = array.Cast<object>().Select(x => x?.ToString());
+            description = string.Join(", ", valueDescriptions);
             return $"[{description}]";
         }
 
+        protected virtual bool TryGetOverridePropertyDescription(string propertyName, object value,
+            out string description)
+        {
+            description = null;
+            return false;
+        }
+
         protected override string Title => ObjectNames.NicifyVariableName(TargetClassType.Name);
+
+        public sealed class SubPageInfo
+        {
+            public SubPageInfo(Type type, Action<(string pageId, DebugPageBase page)> onLoad)
+            {
+                Type = type;
+                OnLoad = onLoad;
+            }
+
+            public Type Type { get; }
+            public Action<(string pageId, DebugPageBase page)> OnLoad { get; }
+        }
     }
 }
