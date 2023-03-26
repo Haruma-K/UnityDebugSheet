4d9b42f974733f9d6b149dd6c2328f239e214f09
Haruki Yano <haruki.y.11g@gmail.com>
2023-03-26 16:29:59 +0900

    Add InputDebugPage.

---
+++ Assets/UnityDebugSheet/Runtime/Extensions/Unity/InputDebugPage.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityDebugSheet.Runtime.Extensions.Unity
+{
+    public sealed class InputDebugPage : PropertyListDebugPageBase<Input>
+    {
+        public override List<string> UpdateTargetPropertyNames { get; } = new List<string>
+        {
+            nameof(Input.mousePosition),
+            nameof(Input.mouseScrollDelta),
+            nameof(Input.compositionCursorPos),
+            nameof(Input.touchCount),
+            nameof(Input.acceleration),
+            nameof(Input.touches)
+        };
+
+        public override Dictionary<string, SubPageInfo> SubPageTargetPropertyNameToInfoMap { get; } =
+            new Dictionary<string, SubPageInfo>
+            {
+                {
+                    nameof(Input.gyro),
+                    new SubPageInfo(typeof(GyroscopeDebugPage), x => ((GyroscopeDebugPage)x.page).Setup(Input.gyro))
+                },
+                {
+                    nameof(Input.compass),
+                    new SubPageInfo(typeof(CompassDebugPage), x => ((CompassDebugPage)x.page).Setup(Input.compass))
+                },
+                {
+                    nameof(Input.location),
+                    new SubPageInfo(typeof(LocationServiceDebugPage),
+                        x => ((LocationServiceDebugPage)x.page).Setup(Input.location))
+                }
+            };
+    }
+}
---
+++ Assets/UnityDebugSheet/Runtime/Extensions/Unity/LocationServiceDebugPage.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace UnityDebugSheet.Runtime.Extensions.Unity
+{
+    public sealed class LocationServiceDebugPage : PropertyListDebugPageBase<LocationService>
+    {
+        private object _targetObject;
+
+        public override BindingFlags BindingFlags => BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance;
+        public override object TargetObject => _targetObject;
+
+
+        public void Setup(LocationService targetObject)
+        {
+            _targetObject = targetObject;
+        }
+
+        protected override bool TryGetOverridePropertyDescription(string propertyName, object value,
+            out string description)
+        {
+            if (propertyName == "lastData")
+            {
+                var lastData = (LocationInfo)value;
+                description = LocationInfoToString(lastData);
+                return true;
+            }
+
+            description = null;
+            return false;
+        }
+
+        private string LocationInfoToString(LocationInfo locationInfo)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("{ ");
+            stringBuilder.Append($"{nameof(LocationInfo.latitude)}: {locationInfo.latitude}");
+            stringBuilder.Append($", {nameof(LocationInfo.longitude)}: {locationInfo.longitude}");
+            stringBuilder.Append($", {nameof(LocationInfo.altitude)}: {locationInfo.altitude}");
+            stringBuilder.Append($", {nameof(LocationInfo.horizontalAccuracy)}: {locationInfo.horizontalAccuracy}");
+            stringBuilder.Append($", {nameof(LocationInfo.verticalAccuracy)}: {locationInfo.verticalAccuracy}");
+            stringBuilder.Append($", {nameof(LocationInfo.timestamp)}: {locationInfo.timestamp}");
+            stringBuilder.Append(" }");
+            return stringBuilder.ToString();
+        }
+    }
+}
